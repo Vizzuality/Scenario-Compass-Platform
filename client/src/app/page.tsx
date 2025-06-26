f05eecad1e6f70d3306522bb5009870bd29fcf04
Andrés González <andres.gonzalez@vizzuality.com>
2025-06-26 18:31:25 +0200

    [SCIIASA-1] application scaffolding

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="grid min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <main className="flex h-full items-center justify-center">
+        <h1 className="text-2xl">Welcome to Scenario Compass Platform</h1>
+      </main>
+    </div>
+  );
+}
