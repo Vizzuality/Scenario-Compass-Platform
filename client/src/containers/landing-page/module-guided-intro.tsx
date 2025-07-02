bff3e5e96d23354036fee2c69e1d594f90b882cc
Peana Florin Cosmin <cosmin.peana@vizzuality.com>
2025-07-02 16:04:07 +0200

    SAIIASA-10 structure for the landing page main content

---
+++ client/src/containers/landing-page/module-guided-intro.tsx
@@ -0,0 +1,55 @@
+import img from "@/assets/images/landing-page/guided-intro.png";
+import Image from "next/image";
+import Link from "next/link";
+
+export function ModuleGuidedIntro() {
+  return (
+    <div
+      className={
+        "bg-beige-light flex flex-col gap-10 px-4 py-16 md:flex-row md:gap-0 lg:px-20 lg:py-28"
+      }
+    >
+      <div className={"order-2 md:order-1 md:w-1/2"}>
+        <p className="pb-6 font-sans text-base leading-6 font-normal text-stone-900 uppercase not-italic">
+          Introduction to Scenarios
+        </p>
+        <p className={"font-display pb-4 text-5xl leading-14 font-bold text-stone-900 not-italic"}>
+          What a climate scenario is
+        </p>
+        <p
+          className={
+            "w-full pb-14 font-sans text-lg leading-7 font-normal text-stone-700 not-italic md:w-4/5"
+          }
+        >
+          Climate scenarios are plausible representations of the future climate of the Earth, based
+          on its current observed state and different greenhouse gas emission scenarios.
+        </p>
+        <div className="flex flex-col flex-wrap items-center gap-5 md:flex-row">
+          <Link
+            href={""}
+            className={
+              "order-2 w-full rounded border-2 border-stone-900 px-8 py-4 text-center font-sans text-base leading-5 font-bold not-italic hover:bg-stone-900/10 md:order-1"
+            }
+          >
+            Methodology overview
+          </Link>
+          <Link
+            href={""}
+            className={
+              "w-full rounded bg-stone-900 px-8 py-4 text-center font-sans text-base leading-5 font-bold text-white not-italic hover:bg-stone-700"
+            }
+          >
+            Key terminology
+          </Link>
+        </div>
+      </div>
+      <div
+        className={
+          "order-1 w-full rounded-xl bg-white px-8 pt-10 pb-7 shadow-lg md:order-2 md:w-1/2"
+        }
+      >
+        <Image src={img} alt={"img"} />
+      </div>
+    </div>
+  );
+}
---
+++ client/src/containers/landing-page/module-share-feedback.tsx
@@ -0,0 +1,3 @@
+export function ModuleShareFeedback() {
+  return <div></div>;
+}
