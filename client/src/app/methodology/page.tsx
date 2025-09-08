66eb7c647821c6c928af8c4bde4b71d987e71072
Andrés González <andres.gonzalez@vizzuality.com>
2025-09-08 09:49:45 +0200

    [SCIIASA-64] adds "coming soon" pages

--- client/src/app/methodology/page.tsx
+++ client/src/app/methodology/page.tsx
@@ -1,14 +1,11 @@
+import { Navbar } from "@/components/layout/navbar/navbar";
+import ComingSoon from "@/containers/coming-soon";
+
 export default function MethodologyPage() {
   return (
-    <main className="flex w-full flex-col items-center">
-      <section className="container flex flex-col gap-16 px-4 py-16 md:px-10 lg:gap-18 lg:px-20 lg:py-28">
-        <div className="flex flex-col items-center gap-10">
-          <h1 className="text-4xl font-bold">Methodology Overview</h1>
-          <p className="text-lg">
-            This page provides an overview of the methodology used in our climate scenarios.
-          </p>
-        </div>
-      </section>
-    </main>
+    <>
+      <Navbar theme="light" sheetTheme="lilac" />
+      <ComingSoon />
+    </>
   );
 }
