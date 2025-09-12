6134d28436fd14b43112fec7f6918613791d3726
Peana Florin Cosmin <cosmin.peana@vizzuality.com>
2025-09-12 18:51:18 +0300

    SAIIASA-72 add unset value

--- client/src/containers/scenario-dashboard/utils/url-store.ts
+++ client/src/containers/scenario-dashboard/utils/url-store.ts
@@ -4,6 +4,8 @@ import { GEOGRAPHY_VALUES } from "@/lib/config/filters/geography-filter-config";
 export const YEAR_OPTIONS = [2000, 2010, 2020, 2030, 2040, 2050, 2060, 2070, 2080, 2090, 2100];
 export const URL_VALUES_FILTER_SEPARATOR = ":";
 
+export const UNSET_FILTER_VALUE = "unset";
+
 export const TOP_FILTER_OPTIONS = {
   YEAR: "year",
   START_YEAR: "startYear",
