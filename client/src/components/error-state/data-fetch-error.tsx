35759a25eb6984c6830ba43f3bf7fbfec05e27df
Peana Florin Cosmin <cosmin.peana@vizzuality.com>
2025-11-13 21:47:38 +0200

    SCIIASA-122

--- client/src/components/error-state/data-fetch-error.tsx
+++ client/src/components/error-state/data-fetch-error.tsx
@@ -1,25 +1,28 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import errorImage from "@/assets/images/warning.webp";
-interface Props {
+
+interface DataFetchErrorProps {
   className?: string;
+  children?: React.ReactNode;
 }
 
-export const DataFetchError = ({ className }: Props) => {
-  return (
-    <div className={cn(className, "flex flex-col items-center justify-center p-4")}>
-      <Image
-        src={errorImage}
-        height={200}
-        width={200}
-        alt="Image displaying the error state of a component"
-      />
+export const DataFetchError = ({ className, children }: DataFetchErrorProps) => {
+  const defaultContent = (
+    <>
       <strong>Unable to load data</strong>
       <p className="text-center">
         The application was unable to retrieve the requested data.
         <br />
         Please try refreshing the page.
       </p>
+    </>
+  );
+
+  return (
+    <div className={cn("flex flex-col items-center justify-center gap-4 p-4", className)}>
+      <Image src={errorImage} height={200} width={200} alt="Error state illustration" priority />
+      {children ?? defaultContent}
     </div>
   );
 };
