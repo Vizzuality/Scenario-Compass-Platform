9096f16a2bc0d26fc8a7a479f69bad8b2e78f6f2
Peana Florin Cosmin <cosmin.peana@vizzuality.com>
2026-01-19 20:00:21 +0200

    IIASA-22

--- client/src/containers/landing-page-container/index.tsx
+++ client/src/containers/landing-page-container/index.tsx
@@ -9,16 +9,17 @@ import ComingSoon from "@/containers/coming-soon-container";
 
 export function LandingPage() {
   const isPrelaunch = env.NEXT_PUBLIC_PRE_LAUNCH_MODE;
+  const hideForLaunch = true;
 
   return (
     <main className="flex w-full flex-col items-center">
       <ModuleHero />
-      {!isPrelaunch && <ModuleGuidedIntro />}
+      {!isPrelaunch || (hideForLaunch && <ModuleGuidedIntro />)}
       <ModuleScenarioDashboard />
-      <ComingSoon />
+      {isPrelaunch && <ComingSoon />}
       {!env.NEXT_PUBLIC_FEATURE_FLAG_HIDE_LEARN_BY_TOPIC_PAGE ||
         (!isPrelaunch && <ModuleLearnByTopic />)}
-      {!isPrelaunch && <ModuleGuidedExploration />}
+      {!isPrelaunch || (hideForLaunch && <ModuleGuidedExploration />)}
       {!isPrelaunch && <ModuleShareFeedback />}
     </main>
   );
