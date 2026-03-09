452bd196fecc4ede2253538357d5b94f2b40c9ed
Peana Florin Cosmin <cosmin.peana@vizzuality.com>
2026-03-09 16:22:43 +0200

    IIASA-64

--- client/src/containers/scenario-dashboard-container/components/filter-top/index.tsx
+++ client/src/containers/scenario-dashboard-container/components/filter-top/index.tsx
@@ -8,7 +8,7 @@ export default function ScenarioDashboardTopFilter({ children }: { children?: Re
     <div className="container mb-10 flex w-full flex-col gap-9 rounded-md pt-6 pb-7">
       <div className="flex w-full flex-col justify-between gap-6 md:flex-row">
         <p className="text-background w-full text-start text-lg leading-7">
-          Start by selecting a country or region and a year.
+          Select region or time period
         </p>
       </div>
       <div className="grid w-full grid-rows-3 items-end gap-6 lg:grid-cols-3 lg:grid-rows-none">
